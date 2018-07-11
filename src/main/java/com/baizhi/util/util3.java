ba1f3982a9278319727b6db69cc7d9dbe01d4477
ql <982656659@qq.com>
2018-07-11 19:46:17 +0800

    dev提交1

---
+++ src/main/java/com/baizhi/util/util3.java
@@ -0,0 +1,10 @@
+package com.baizhi.util;
+
+/**
+ * Created by qiao on 2018/7/11.
+ */
+public class util3 {
+    public static void main(String[] args) {
+        System.out.println("aaaaa");
+    }
+}
